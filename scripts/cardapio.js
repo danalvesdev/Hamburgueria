141e6bc0d78f52160362a4fcc7cc5403312af5d8
Daniel Alves <da445689@gmail.com>
2026-04-14 20:37:34 -0300

    alguns ajuste de letra, botões,tamanhos

--- scripts/cardapio.js
+++ scripts/cardapio.js
@@ -4,8 +4,8 @@ let html = "";
     html += `<h2>Cardápio</h2>`;
     for(let i = 0; i < nomes.length; i++){
     html += `
-    <div id="burguer-card">
-      <div class="burguer-cardapio">
+    <div id="destaques">
+      <div class="burguer-semanal">
         <img src= "${imagens[i]}" alt="${nomes[i]}" class="foto-burguer">
         <div class="titulo">
             <h2>${nomes[i]}</h2>
